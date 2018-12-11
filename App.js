cd5059d6e2ec3d7bd274cb8b9bc55d17cd53c396
Lingesh <lingeshwaran001@gmail.com>
2018-12-11 09:33:10 +0530

    DemoLogin

---
+++ App.js
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state={}
+    this.buttonPressed=this.buttonPressed.bind(this)
+  }
+
+    buttonPressed(){
+      //const username= this._username._lastNativeText;
+      //const password= this._password._lastNativeText;
+      console.log(this.state.username, this.state.password)
+
+    }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Username</Text>
+        <TextInput underlineColorAndroid='#000000' 
+          defaultValue={this.state.username}
+          onChangeText={text => this.setState({username: text})}
+        />
+        <Text>Password</Text>
+        <TextInput underlineColorAndroid='#000000' 
+          defaultValue={this.state.password}
+          onChangeText={text => this.setState({password: text})}
+        />
+
+        <Button title={"Login"} onPress={this.buttonPressed} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20
+  }
+});
